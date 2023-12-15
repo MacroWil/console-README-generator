009c50e29da10525fefb83a1d4c4cce0a5e86154
macrowil <macrow.co@gmail.com>
2023-12-15 12:44:17 -0700

    added majority of generate readme code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,46 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const sections = [
+    "Description",
+    "Installation",
+    "Contributing",
+    "Usage",
+    "Tests",
+    "License",
+    "Questions",
+  ];
+  //title
+  let readme = `# ${data.title}\n`;
+  //table of contents
+  readme += "## Table of Contents\n\n";
+  for (const section of sections) {
+    readme += `- [${section}](#${section})\n`;
+  }
+  readme += "\n";
+  //description
+  readme += `##${sections[0]}\n\n`;
+  readme += `${data.desc}\n\n`;
+  //installation
+  readme += `##${sections[1]}\n\n`;
+  readme += `${data.install}\n\n`;
+  //contributors
+  readme += `##${sections[2]}\n\n`;
+  readme += `${data.contribute}\n\n`;
+  //usage
+  readme += `##${sections[3]}\n\n`;
+  readme += `${data.usage}\n\n`;
+  //tests
+  readme += `##${sections[4]}\n\n`;
+  readme += `${data.test}\n\n`;
+  //license
+  readme += `##${sections[5]}\n\n`;
+  readme += `${data.license}\n\n`;
+  //questions
+  readme += `##${sections[6]}\n\n`;
+  readme += `Additional questions?\nYou can find me [HERE](https://github.com/${data.username}" on GitHub.\nOr, you can email me at ${data.email}.\n\n`;
 
-`;
+  return readme;
 }
 
 module.exports = generateMarkdown;
