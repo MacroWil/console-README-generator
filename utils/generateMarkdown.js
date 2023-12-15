73a546acba1143d5acd2ddbacca9aefe05622aa7
macrowil <macrow.co@gmail.com>
2023-12-15 13:52:09 -0700

    added a generated readme, updated generate markdown to include all license information in generation including bade, added gitignore for generated folder

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+
+  if (license != "None") {
+    badge = `![License Badge](https://shields.io/badge/license-${license}-green)`;
+  }
+
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = "";
+
+  // select correct license link for the selected license
+  switch (license) {
+    case "BSD":
+      licenseLink = "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+    case "MIT":
+      licenseLink = "https://mit-license.org/";
+      break;
+    case "Apache":
+      licenseLink = "https://www.apache.org/licenses/LICENSE-2.0.html";
+      break;
+    case "GPL":
+      licenseLink = "https://www.gnu.org/licenses/gpl-3.0.en.html";
+      break;
+    default:
+      licenseLink = "";
+      break;
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = "";
+
+  if (license != "None") {
+    licenseSection += `##License\n\n`;
+    licenseSection += `See ${renderLicenseLink(
+      license
+    )} for more information about licensing.\n\n`;
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -23,10 +63,14 @@ function generateMarkdown(data) {
   ];
   //title
   let readme = `# ${data.title}\n\n`;
+  //license badge
+  readme += `${renderLicenseBadge(data.license)}\n\n`;
   //table of contents
   readme += "## Table of Contents\n\n";
   for (const section of sections) {
-    readme += `- [${section}](#${section})\n`;
+    if (!(section === "License" && data.license === "None")) {
+      readme += `- [${section}](#${section})\n`;
+    }
   }
   readme += "\n";
   //description
@@ -45,8 +89,7 @@ function generateMarkdown(data) {
   readme += `##${sections[4]}\n\n`;
   readme += `${data.test}\n\n`;
   //license
-  readme += `##${sections[5]}\n\n`;
-  readme += `${data.license}\n\n`;
+  readme += `${renderLicenseSection(data.license)}\n`;
   //questions
   readme += `##${sections[6]}\n\n`;
   readme += `Additional questions?\nYou can find me [HERE](https://github.com/${data.username}) on GitHub.\nOr, you can email me at ${data.email}.\n\n`;
